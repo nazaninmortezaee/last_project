225f02066033afb458bbb14358503bbee95e84c1
Nazanin <mortezaeeenazanin@gmail.com>
2024-06-03 19:21:35 +0430

    axa va jump eafe shod

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ print("main.py is being run")
 
 import pygame
 import sys
+from constants import *
 from nazi import Nazi
+
 
 class Game:
     fps = 60
@@ -10,22 +12,61 @@ class Game:
         pygame.init()
         self.game_display = pygame.display.set_mode((1000, 600))
         pygame.display.set_caption("Nazi Game")
-
-        self.player1 = Nazi((500, 300), self.fps)
+        self.font = pygame.font.Font(None, 22)
+        self.high_score = self.load_high_score()
+        self.score = 0
+        self.player1 = Nazi((100, 410), self.fps)
         self.clock = pygame.time.Clock()
-
         self.run()
 
+
+    def load_high_score(self):
+        with open("save.txt", "r") as file:
+            high_score = file.read()
+            return high_score
+
+    def show_score(self):
+        score = str(self.score)
+        _score = self.font.render((6 - len(score))* "0" + score , True, WHITE)
+        _hscore =  self.font.render("Hsc " + self.high_score, True, WHITE)
+        self.game_display.blit(_score, (890, 40))
+        self.game_display.blit(_hscore, (770, 40))
+    
+    def best_score(self):
+        if int(self.high_score) < self.score:
+            with open("save.txt", "w") as file:
+                _str = (6 - len(str(self.score)))* "0" + str(self.score)
+                file.write(_str)
+
+
+
     def run(self):
+        rect1= pygame.Rect(500, 420, 30, 30)
+        ground = pygame.image.load("./assets/desert.png")
         while True:
             self.game_display.fill((0, 0, 0))
+            self.show_score()
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
-
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_SPACE:
+                        self.player1.jump()
+            
+            rect1.x -= 3
+            pygame.draw.rect(self.game_display, (255, 255, 255), rect1)
+            if rect1.colliderect(self.player1.rect):
+                print("Collition!")
+                self.best_score()
+                pygame.quit()
+                sys.exit()
+                                 
+           
+            self.game_display.blit(ground, (0,450))
             self.player1.update(self.game_display)
             pygame.display.update()
+            self.score += 1
             self.clock.tick(self.fps)
 
 new_game = Game()
--- nazi.py
+++ nazi.py
@@ -12,20 +12,40 @@ class Nazi:
     def __init__(self, location, fps):
         self.location = location
         self.x_loc, self.y_loc = location
+        self.Y_LOC = self.y_loc
         self.fps = fps
         self.w_img = True
         self.w_slicer = 8
         self.w_counter = 0
+        self.state = True
+        self.j_threshold = self.y_loc - 250
+        self.YD = 5 #ydirection hamishegi
+        self.yd = self.YD
+        self.rect = self.i_walk1.get_rect(topleft = self.location)
 
     def update(self, surface):
-        if self.w_counter < self.fps:
-            if self.w_counter % self.w_slicer == 0:
-                self.w_img = not self.w_img
-            self.w_counter += 1
+        if self.state:
+            if self.w_counter < self.fps:
+                if self.w_counter % self.w_slicer == 0:
+                    self.w_img = not self.w_img
+                self.w_counter += 1
+            else:
+                self.w_counter = 0
+            if self.w_img:
+                surface.blit(self.i_walk1, self.location)
+            else:
+                surface.blit(self.i_walk2, self.location)
         else:
-            self.w_counter = 0
-        if self.w_img:
-            surface.blit(self.i_walk1, self.location)
-        else:
-            surface.blit(self.i_walk2, self.location)
+            if self.yd > 0 : #going up
+                if self.y_loc < self.j_threshold:
+                    self.yd *= -1
+            else: #going down
+                if self.y_loc > self.Y_LOC:
+                    self.state = True
+                    self.yd = self.YD
+            self.y_loc -= self.yd
+            surface.blit(self.i_jump, (self.x_loc, self.y_loc))
+            self.rect.topleft = (self.x_loc, self.y_loc)
+    def jump(self):
+        self.state = False                
         
